71d4ba08f8e66e4b17b5eefcff9cb5dce265230d
HamkaArifani <hamka0897@gmail.com>
2026-06-16 22:07:09 +0800

    feat: implement MusicRepository

--- Tugas Rest-API/app/src/main/java/com/example/mockapi/data/repository/MusicRepository.kt
+++ Tugas Rest-API/app/src/main/java/com/example/mockapi/data/repository/MusicRepository.kt
@@ -1,4 +1,12 @@
 package com.example.mockapi.data.repository
 
-class MusicRepository() {
+import com.example.mockapi.data.dto.toDomain
+import com.example.mockapi.data.remote.ApiService
+import com.example.mockapi.model.Music
+
+class MusicRepository(private val apiService: ApiService){
+
+    suspend fun getMusicDetail(): Music {
+        return apiService.getMusic().data.toDomain()
+    }
 }
--- Tugas Rest-API/app/src/main/java/com/example/mockapi/ui/fragment/MusicViewModel.kt
+++ Tugas Rest-API/app/src/main/java/com/example/mockapi/ui/fragment/MusicViewModel.kt
@@ -1,4 +1,5 @@
 package com.example.mockapi.ui.fragment
 
 class MusicViewModel {
+
 }
